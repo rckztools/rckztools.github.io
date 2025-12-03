2188f0932c92371575d394749e407d9467b2f19e
rckztools <rckztools@gmail.com>
2025-12-04 00:19:28 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,4 @@
+document.getElementById('year')?.textContent = new Date().getFullYear();
+document.getElementById('year2')?.textContent = new Date().getFullYear();
+document.getElementById('year3')?.textContent = new Date().getFullYear();
+document.getElementById('year4')?.textContent = new Date().getFullYear();
